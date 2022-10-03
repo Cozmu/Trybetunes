3e89b929a439c41d0e3328310fd44a5e46c4591f
Cozmu <www.jorgereis@gmail.com>
2022-10-03 20:38:46 -0300

    Adicionando requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,49 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    licenseBtn: true,
+    artistName: '',
+  };
+
+  enableBtn = () => {
+    const { artistName } = this.state;
+    const two = 2;
+    const validation = artistName.length >= two;
+    this.setState({ licenseBtn: !validation });
+  };
+
+  onInputChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    }, () => this.enableBtn());
+  };
+
   render() {
+    const { licenseBtn, artistName } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search-artist">
+            <input
+              id="search-artist"
+              type="text"
+              name="artistName"
+              value={ artistName }
+              data-testid="search-artist-input"
+              onChange={ this.onInputChange }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            onClick={ () => {} }
+            disabled={ licenseBtn }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
