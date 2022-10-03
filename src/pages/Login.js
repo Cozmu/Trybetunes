09bad010fe9b80d15cc16b530e0b147e3167f506
Cozmu <www.jorgereis@gmail.com>
2022-10-03 18:07:34 -0300

    Adicionando requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { createUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Login extends React.Component {
+  state = {
+    license: true,
+    usuario: '',
+    loading: false,
+  };
+
+  enableBtn = () => {
+    const { usuario } = this.state;
+    const three = 3;
+    const validation = usuario.length >= three;
+    this.setState({ license: !validation });
+  };
+
+  onInputChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    }, () => this.enableBtn());
+  };
+
+  submite = async () => {
+    const { history } = this.props;
+    const { usuario } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name: usuario });
+    history.push('/search');
+  };
+
   render() {
+    const { license, usuario, loading } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        {loading ? <Carregando />
+          : <div>
+            <label htmlFor="name-input">
+              <input
+                id="name-input"
+                name="usuario"
+                data-testid="login-name-input"
+                onChange={ this.onInputChange }
+                value={ usuario }
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ license }
+              onClick={ this.submite }
+            >
+              Entrar
+            </button>
+            </div>}
+      </div>
     );
   }
 }
