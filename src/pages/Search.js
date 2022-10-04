dd02c9e5cb7aa212b666423f118b1a60007dfecc
Cozmu <www.jorgereis@gmail.com>
2022-10-03 22:35:32 -0300

    Inciando requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from './Carregando';
 
 class Search extends React.Component {
   state = {
     licenseBtn: true,
     artistName: '',
+    loading: false,
+    fraseDeBusca: 
+    // songs: [],
   };
 
   enableBtn = () => {
@@ -20,31 +25,47 @@ class Search extends React.Component {
     }, () => this.enableBtn());
   };
 
+  searchArtist = async () => {
+    const { artistName } = this.state;
+    this.setState({
+      artistName: '',
+      loading: true,
+    });
+    const request = await searchAlbumsAPI(artistName);
+    this.setState({
+      loading: false,
+      songs: request,
+    });
+    console.log(request);
+  };
+
   render() {
-    const { licenseBtn, artistName } = this.state;
+    const { licenseBtn, artistName, loading, songs } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <label htmlFor="search-artist">
-            <input
-              id="search-artist"
-              type="text"
-              name="artistName"
-              value={ artistName }
-              data-testid="search-artist-input"
-              onChange={ this.onInputChange }
-            />
-          </label>
-          <button
-            data-testid="search-artist-button"
-            type="button"
-            onClick={ () => {} }
-            disabled={ licenseBtn }
-          >
-            Pesquisar
-          </button>
-        </form>
+        { loading ? <Carregando />
+          : (
+            <form>
+              <label htmlFor="search-artist">
+                <input
+                  id="search-artist"
+                  type="text"
+                  name="artistName"
+                  value={ artistName }
+                  data-testid="search-artist-input"
+                  onChange={ this.onInputChange }
+                />
+              </label>
+              <button
+                data-testid="search-artist-button"
+                type="button"
+                onClick={ this.searchArtist }
+                disabled={ licenseBtn }
+              >
+                Pesquisar
+              </button>
+            </form>)}
       </div>
     );
   }
