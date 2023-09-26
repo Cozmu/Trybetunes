bd19ca880177b321a79679537184d4d2dac3a953
Cozmu <www.jorgereis@gmail.com>
2023-09-26 19:18:42 -0300

    style: adicionando estilização de pagina de pergil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { IoPersonCircleOutline } from 'react-icons/io5';
 import { NavLink } from 'react-router-dom';
 import Header from '../components/Header';
 import Carregando from '../components/Carregando';
 import { getUser } from '../services/userAPI';
+import style from '../styles/profile.module.scss';
 
 class Profile extends React.Component {
   state = {
     isLoading: false,
-    userApi: [],
+    name: '',
+    image: '',
+    email: '',
+    description: '',
   };
 
   async componentDidMount() {
@@ -15,35 +20,61 @@ class Profile extends React.Component {
       isLoading: true,
     });
     const request = await getUser();
+    console.log(request);
     this.setState({
       isLoading: false,
-      userApi: [request],
+      name: request.name,
+      image: request.image,
+      email: request.email,
+      description: request.description,
     });
   }
 
   render() {
-    const { isLoading, userApi } = this.state;
+    const { isLoading, name, email, image, description } = this.state;
     return (
-      <div data-testid="page-profile">
+      <div
+        className={ style.main_container }
+        data-testid="page-profile"
+      >
         <Header />
-        {isLoading ? <Carregando />
-          : userApi.map((e, i) => (
-            <section key={ i }>
-              <h4>PERFIL</h4>
-              <img
-                data-testid="profile-image"
-                src={ e.image }
-                alt={ e.name }
-              />
-              <p><b>Nome</b></p>
-              <p>{e.name}</p>
-              <p><b>E-mail</b></p>
-              <p>{e.email}</p>
-              <p><b>Descrição</b></p>
-              <p>{e.description}</p>
-              <NavLink to="/profile/edit">Editar perfil</NavLink>
+        {isLoading
+          ? (
+            <section className={ style.loading_container }>
+              <Carregando />
             </section>
-          ))}
+          )
+          : (
+            <main>
+              <div className={ style.photo_container }>
+                {image ? <img
+                  data-testid="profile-image"
+                  src={ image }
+                  alt={ name }
+                /> : <IoPersonCircleOutline className={ style.profile_not_found } />}
+              </div>
+              <div className={ style.informations_container }>
+                <div>
+                  <h3>Nome</h3>
+                  <p>{name}</p>
+                </div>
+                <div>
+                  {email && <h3>E-mail</h3>}
+                  <p>{email}</p>
+                </div>
+                <div>
+                  {description && <h3>Descrição</h3>}
+                  <p>{description}</p>
+                </div>
+                <NavLink
+                  className={ style.redirect_button }
+                  to="/profile/edit"
+                >
+                  EDITAR PERFIL
+                </NavLink>
+              </div>
+            </main>
+          )}
       </div>
     );
   }
